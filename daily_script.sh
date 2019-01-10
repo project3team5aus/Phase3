6aaf5dc1b094f2b444e715790c5b7fa57ae1957b
LCC2525 <christian.coleman3@g.austincc.edu>
2019-01-09 22:49:18 -0600

    update sqlite after game_predictions.py was run

--- daily_script.sh
+++ daily_script.sh
@@ -14,7 +14,7 @@ echo Start of script to push sqlite updates to GitHub
 cd /c/Users/lcc25/repos/Phase3
 git add .
 sleep 1
-git commit -am "update sqlite after game_predictions.py was run-lcc"
+git commit -am "update sqlite after game_predictions.py was run"
 sleep 2
 git push -u origin master
 sleep 15
